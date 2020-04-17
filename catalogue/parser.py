fb2f9af19c07ca18216b8feac076b8b56f1eb56b
sgibson91 <sgibson@turing.ac.uk>
2020-04-17 09:17:18 +0100

    Wrap parser in main function so can be used as console entry point

--- catalogue/parser.py
+++ catalogue/parser.py
@@ -2,40 +2,47 @@ import argparse
 from engage import engage, disengage
 from compare import compare, checkhashes
 
-parser = argparse.ArgumentParser(
-    description="",
-    formatter_class=argparse.RawTextHelpFormatter)
+
+def main():
+    """Main function"""
+    parser = argparse.ArgumentParser(
+        description="",
+        formatter_class=argparse.RawTextHelpFormatter)
 
 
-parser.add_argument(
-    '--output_data',
-    type=list,
-    metavar='output_directories',
-    help=textwrap.dedent(""),
-    default=None)
+    parser.add_argument(
+        '--output_data',
+        type=list,
+        metavar='output_directories',
+        help=textwrap.dedent(""),
+        default=None)
 
-parser.add_argument(
-    '--input_data',
-    type=str,
-    metavar='input_data',
-    help=textwrap.dedent(""),
-    default=None)
+    parser.add_argument(
+        '--input_data',
+        type=str,
+        metavar='input_data',
+        help=textwrap.dedent(""),
+        default=None)
 
-parser.add_argument(
-    '--code',
-    type=str,
-    metavar='input_data',
-    help=textwrap.dedent(""),
-    default=None)
+    parser.add_argument(
+        '--code',
+        type=str,
+        metavar='input_data',
+        help=textwrap.dedent(""),
+        default=None)
 
-compare_parser = parser.add_subparsers("compare", description="", help="")
-compare_parser.set_defaults(func=engage)
+    compare_parser = parser.add_subparsers("compare", description="", help="")
+    compare_parser.set_defaults(func=engage)
 
-checkhashes_parser = parser.add_subparsers("checkhashes", description="", help="")
-checkhashes_parser.set_defaults(func=disengage)
+    checkhashes_parser = parser.add_subparsers("checkhashes", description="", help="")
+    checkhashes_parser.set_defaults(func=disengage)
 
-engage_parser = parser.add_subparsers("engage", description="", help="")
-engage_parser.set_defaults(func=compare)
+    engage_parser = parser.add_subparsers("engage", description="", help="")
+    engage_parser.set_defaults(func=compare)
 
-disengage_parser = parser.add_subparsers("disengage", description="", help="")
-disengage_parser.set_defaults(func=checkhashes)
+    disengage_parser = parser.add_subparsers("disengage", description="", help="")
+    disengage_parser.set_defaults(func=checkhashes)
+
+
+if __name__ == "__main__":
+    main()
