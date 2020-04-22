390cc7852eac4dbc7ff9918d6ccbbdc88b7ec536
radka-j <r.jersakova@gmail.com>
2020-04-22 16:12:04 +0200

    fix subparser args and names

--- catalogue/parser.py
+++ catalogue/parser.py
@@ -13,38 +13,52 @@ def main():
 
     subparsers = parser.add_subparsers()
 
-    parser.add_argument(
-        '--output_data',
-        type=list,
-        metavar='output_directories',
-        help=textwrap.dedent(""),
-        default=None)
-
-    parser.add_argument(
+    engage_parser = subparsers.add_parser("engage", description="", help="")
+    engage_parser.set_defaults(func=engage)
+    
+    engage_parser.add_argument(
         '--input_data',
         type=str,
         metavar='input_data',
         help=textwrap.dedent(""),
         default=None)
 
-    parser.add_argument(
+    engage_parser.add_argument(
         '--code',
         type=str,
         metavar='input_data',
         help=textwrap.dedent(""),
         default=None)
 
-    compare_parser = subparsers.add_parser("engage", description="", help="")
-    compare_parser.set_defaults(func=engage)
-
     checkhashes_parser = subparsers.add_parser("checkhashes", description="", help="")
     checkhashes_parser.set_defaults(func=check_hashes)
 
-    engage_parser = subparsers.add_parser("compare", description="", help="")
-    engage_parser.set_defaults(func=compare)
+    compare_parser = subparsers.add_parser("compare", description="", help="")
+    compare_parser.set_defaults(func=compare)
 
     disengage_parser = subparsers.add_parser("disengage", description="", help="")
     disengage_parser.set_defaults(func=disengage)
+
+    disengage_parser.add_argument(
+        '--input_data',
+        type=str,
+        metavar='input_data',
+        help=textwrap.dedent(""),
+        default=None)
+
+    disengage_parser.add_argument(
+        '--code',
+        type=str,
+        metavar='input_data',
+        help=textwrap.dedent(""),
+        default=None)
+
+    disengage_parser.add_argument(
+        '--output_data',
+        type=list,
+        metavar='output_directories',
+        help=textwrap.dedent(""),
+        default=None)
 
     parser.parse_args()
 
