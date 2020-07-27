166ab4631047bcee586001578b5b18d62df684f8
Kevin Xu <kevin.xu@faststream.civilservice.gov.uk>
2020-07-27 14:08:00 +0100

    added two tests to test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,13 @@
 
 import pytest
 
-from catalogue.utils import check_paths_exists, create_timestamp
+from catalogue.utils import check_paths_exists, create_timestamp, read_config_file, dictionary_printer
 
 
 def test_create_timestamp():
 
     assert type(create_timestamp()) == str
-    
+
     assert len(create_timestamp()) == 15
 
 
@@ -24,3 +24,18 @@ def test_check_paths_exists(test_args):
     # path is not a valid type
     setattr(test_args, "output_data", 123)
     assert check_paths_exists(test_args) == False
+
+def test_read_config_file(fixture5):
+    dict = read_config_file(fixture5)
+    assert dict['code'] == 'code'
+    assert dict['input_data'] == 'input_data'
+    assert dict['csv'] == None
+
+
+
+def test_dictionary_printer(capsys):
+    dict = {'a': 'hi', 'b' :'bye'}
+    dictionary_printer(dict)
+    captured = capsys.readouterr()
+
+    assert 'a:hi\nb:bye' in captured.out
